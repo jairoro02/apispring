3d86e975023a4e5746a3eb1ba1c9cf10c75c507c
jairoro <jromero.alumno@iesrafaelalberti.es>
2023-02-06 14:25:23 +0100

    controladores

--- src/main/java/es/iesrafaelalberti/apicliente/controllers/HeroeController.java
+++ src/main/java/es/iesrafaelalberti/apicliente/controllers/HeroeController.java
@@ -1,12 +1,13 @@
 package es.iesrafaelalberti.apicliente.controllers;
 
+import es.iesrafaelalberti.apicliente.models.Heroe;
 import es.iesrafaelalberti.apicliente.repositories.HeroesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class HeroeController {
@@ -22,4 +23,29 @@ public class HeroeController {
         return new ResponseEntity<>(heroesRepository.findById(id), HttpStatus.OK);
     }
 
+    @GetMapping("/heroes/create/")
+    public ResponseEntity<Object> create(@RequestBody Heroe heroe){
+        heroesRepository.save(heroe);
+        return new ResponseEntity<>(heroe, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/heroes/{id}/")
+    public ResponseEntity<Object> delete(@PathVariable("id") Long id){
+        Optional<Heroe> heroe =heroesRepository.findById(id);
+        heroe.ifPresent(value -> heroesRepository.delete(value));
+        return new ResponseEntity<>(heroe.isPresent(),HttpStatus.OK);
+    }
+
+    @PutMapping("/heroes/{id}/")
+    public ResponseEntity<Object> update(@PathVariable("id") Long id, @RequestBody Heroe nuevoheroe){
+        Optional<Heroe> heroe =heroesRepository.findById(id);
+        if(heroe.isPresent()){
+            nuevoheroe.setId(id);
+            heroesRepository.save(nuevoheroe);
+            return new ResponseEntity<>(nuevoheroe,HttpStatus.OK);
+
+
+        }
+        return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
+    }
 }
--- src/main/java/es/iesrafaelalberti/apicliente/controllers/VillanoController.java
+++ src/main/java/es/iesrafaelalberti/apicliente/controllers/VillanoController.java
@@ -1,9 +1,43 @@
 package es.iesrafaelalberti.apicliente.controllers;
 
+import es.iesrafaelalberti.apicliente.models.Villano;
+import es.iesrafaelalberti.apicliente.repositories.VillanosRepository;
 import jakarta.persistence.ManyToOne;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
+@RestController
 public class VillanoController {
 
+    @Autowired
+    VillanosRepository villanosRepository;
+
+    @GetMapping("/")
+    public ResponseEntity<Object> index(){return new ResponseEntity<>(villanosRepository.findAll(), HttpStatus.OK);}
+
+    @GetMapping("/villanos/{id}/")
+    public ResponseEntity<Object> show(@PathVariable("id") Long id){
+        return new ResponseEntity<>(villanosRepository.findById(id),HttpStatus.OK);
+    }
+
+    @GetMapping("/villanos/create/")
+    public ResponseEntity<Object> create(@RequestBody Villano villano){
+        villanosRepository.save(villano);
+        return new ResponseEntity<>(villano, HttpStatus.OK);
+    }
+
+    @PutMapping("/villanos/{id}/")
+    public ResponseEntity<Object> update(@PathVariable("id")  @RequestBody Villano villano){
+        Optional<Villano> villano1 = villanosRepository.findById(id);
+
+    }
 
 
 
