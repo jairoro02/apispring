474b1a7f28663de7c59f42a5b82cb087f171f860
jairoro <jromero.alumno@iesrafaelalberti.es>
2023-03-21 13:27:12 +0100

    Heroe DTO

---
+++ src/main/java/es/iesrafaelalberti/apicliente/dto/VillanoCreateDTO.java
@@ -0,0 +1,15 @@
+package es.iesrafaelalberti.apicliente.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class VillanoCreateDTO implements Serializable {
+}
